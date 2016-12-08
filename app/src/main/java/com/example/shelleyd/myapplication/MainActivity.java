3749525c9c6a3edbc1c6ca6aaa87a881c70c3bf7
ColidoUK <JoeB@print-rite-europe.com>
2016-12-08 16:35:19 +0000

    Changes ImageViews to ImageButtons Added onClickListeners for each button to display a toast message when pressed

--- app/src/main/java/com/example/shelleyd/myapplication/MainActivity.java
+++ app/src/main/java/com/example/shelleyd/myapplication/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +20,55 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //Initialise buttons
+        ImageButton diyButton = (ImageButton) findViewById(R.id.diyImage);
+        ImageButton compactButton = (ImageButton) findViewById(R.id.compactImage);
+        ImageButton twoPlusButton = (ImageButton) findViewById(R.id.twoplusImage);
+        ImageButton m2020Button = (ImageButton) findViewById(R.id.m2020Image);
+        ImageButton x3045Button = (ImageButton) findViewById(R.id.x3045Image);
+
+        //Set what happens when a user presses a button
+        diyButton.setOnClickListener(diyOnClick);
+        compactButton.setOnClickListener(compactOnClick);
+        twoPlusButton.setOnClickListener(twoPlusOnClick);
+        m2020Button.setOnClickListener(m2020OnClick);
+        x3045Button.setOnClickListener(x3045OnClick);
     }
+
+    View.OnClickListener diyOnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(getApplicationContext(), "DIY Button Pressed", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    View.OnClickListener compactOnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(getApplicationContext(), "Compact Button Pressed", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    View.OnClickListener twoPlusOnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(getApplicationContext(), "2+ Button Pressed", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    View.OnClickListener m2020OnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(getApplicationContext(), "M2020 Button Pressed", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    View.OnClickListener x3045OnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(getApplicationContext(), "X3045 Button Pressed", Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
