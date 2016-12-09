7f72221953a0e91e0f6dc148eb0ca2c0871d3dc8
ColidoUK <JoeB@print-rite-europe.com>
2016-12-09 14:30:55 +0000

    Added Second Page - Main screen after printer selection Carried name of selected printer over to the new activity

--- app/src/main/java/com/example/shelleyd/myapplication/MainActivity.java
+++ app/src/main/java/com/example/shelleyd/myapplication/MainActivity.java
@@ -1,15 +1,12 @@
 package com.example.shelleyd.myapplication;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ImageButton;
-import android.widget.Toast;
+        import android.os.Bundle;
+        import android.support.design.widget.FloatingActionButton;
+        import android.support.design.widget.Snackbar;
+        import android.support.v7.app.AppCompatActivity;
+        import android.support.v7.widget.Toolbar;
+        import android.view.View;
+        import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,75 +17,24 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //Initialise buttons
-        ImageButton diyButton = (ImageButton) findViewById(R.id.diyImage);
-        ImageButton compactButton = (ImageButton) findViewById(R.id.compactImage);
-        ImageButton twoPlusButton = (ImageButton) findViewById(R.id.twoplusImage);
-        ImageButton m2020Button = (ImageButton) findViewById(R.id.m2020Image);
-        ImageButton x3045Button = (ImageButton) findViewById(R.id.x3045Image);
+        TextView printerText = (TextView) findViewById(R.id.textView);
 
-        //Set what happens when a user presses a button
-        diyButton.setOnClickListener(diyOnClick);
-        compactButton.setOnClickListener(compactOnClick);
-        twoPlusButton.setOnClickListener(twoPlusOnClick);
-        m2020Button.setOnClickListener(m2020OnClick);
-        x3045Button.setOnClickListener(x3045OnClick);
+        String printerName;
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if (extras == null) {
+                printerName = null;
+            }
+            else {
+                printerName = extras.getString("PRINTER_NAME");
+            }
+        }
+        else {
+            printerName = (String) savedInstanceState.getSerializable("PRINTER_NAME");
+        }
+
+        printerText.setText(printerName);
+
     }
 
-    View.OnClickListener diyOnClick = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Toast.makeText(getApplicationContext(), "DIY Button Pressed", Toast.LENGTH_SHORT).show();
-        }
-    };
-
-    View.OnClickListener compactOnClick = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Toast.makeText(getApplicationContext(), "Compact Button Pressed", Toast.LENGTH_SHORT).show();
-        }
-    };
-
-    View.OnClickListener twoPlusOnClick = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Toast.makeText(getApplicationContext(), "2+ Button Pressed", Toast.LENGTH_SHORT).show();
-        }
-    };
-
-    View.OnClickListener m2020OnClick = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Toast.makeText(getApplicationContext(), "M2020 Button Pressed", Toast.LENGTH_SHORT).show();
-        }
-    };
-
-    View.OnClickListener x3045OnClick = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Toast.makeText(getApplicationContext(), "X3045 Button Pressed", Toast.LENGTH_SHORT).show();
-        }
-    };
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
