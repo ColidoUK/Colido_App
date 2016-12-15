cff8bb1be8fa2f5385a4e26b792127bb89117a84
ColidoUK <JoeB@print-rite-europe.com>
2016-12-15 16:50:39 +0000

    Added custom ListView to second page which is a list of images.

--- app/src/main/java/com/example/shelleyd/myapplication/MainActivity.java
+++ app/src/main/java/com/example/shelleyd/myapplication/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.shelleyd.myapplication;
 
+        import android.content.Context;
+        import android.media.Image;
         import android.os.Bundle;
+        import android.support.annotation.NonNull;
+        import android.support.annotation.Nullable;
         import android.support.design.widget.FloatingActionButton;
         import android.support.design.widget.Snackbar;
         import android.support.v7.app.AppCompatActivity;
         import android.support.v7.widget.Toolbar;
+        import android.util.Log;
+        import android.view.LayoutInflater;
         import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.ArrayAdapter;
+        import android.widget.ImageView;
+        import android.widget.ListView;
+        import android.widget.ScrollView;
         import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int[]images={R.drawable.high_res_image,/*R.drawable.compactfront,R.drawable.front2plus,R.drawable.m2020,R.drawable.x3045*/ R.drawable.high_res_image, R.drawable.high_res_image};
+
+    ListView myListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,24 +32,57 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        TextView printerText = (TextView) findViewById(R.id.textView);
+        myListView = (ListView) findViewById(R.id.myList);
+        MyAdapter adapter = new MyAdapter (MainActivity.this, images);
+        myListView.setAdapter(adapter);
 
-        String printerName;
-        if (savedInstanceState == null) {
-            Bundle extras = getIntent().getExtras();
-            if (extras == null) {
-                printerName = null;
+    }
+
+    class MyAdapter extends ArrayAdapter<String> {
+
+        Context context;
+        int[]images;
+        int count = 0;
+
+        public  MyAdapter(Context context, int imgs[]) {
+            super(MainActivity.this, R.layout.image_list_row);
+            this.context = MainActivity.this;
+            this.images = imgs;
+        }
+
+        @Override
+        public int getCount() {
+            return images.length;
+        }
+
+        class ViewHolder {
+
+            private ImageView myImage;
+        }
+        @Override
+        @NonNull
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder myViewHolder;
+
+            if (convertView == null) {
+
+                myViewHolder = new ViewHolder();
+
+                LayoutInflater myInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = myInflater.inflate(R.layout.image_list_row, parent, false);
+                myViewHolder.myImage = (ImageView) convertView.findViewById(R.id.listImage);
+                convertView.setTag(myViewHolder);
             }
             else {
-                printerName = extras.getString("PRINTER_NAME");
+                myViewHolder = (ViewHolder) convertView.getTag();
             }
-        }
-        else {
-            printerName = (String) savedInstanceState.getSerializable("PRINTER_NAME");
-        }
+            Log.d("Plates", "Count: " + count++);
 
-        printerText.setText(printerName);
+            myViewHolder.myImage.setImageResource(images[position]);
 
+            return convertView;
+        }
     }
 
 }
